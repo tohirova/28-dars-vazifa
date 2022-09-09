05aad54789dcd4962547f15e5daee3add0a3f4d5
tohirova <tohirovaziyodaxon5@gmail.com>
2022-09-09 12:14:51 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+//Xarajatlar hisoblagichi
+
+const plane = 500;
+const hotel = 250;
+const travel = 120;
+const dollor = 10650.34;
+const yevro = 11650.03;
+const price = (plane + hotel) * dollor;
+alert(price);
+const price2 = travel * yevro;
+const totalAmount = price + price2;
+console.log('Umumiy summa ' + totalAmount + " so'm")
+
+
+//Matematik amal uchun mashqlar
+
+// 1 - misol
+
+const a = 10;
+const b = 5;
+console.log(a + b);
+
+
+// 2 - misol
+
+const c = 25;
+const d = 15;
+const result = c - d;
+console.log(result);
+
+
+
+// 3 - misol
+
+
+const e = 10;
+const f = 5;
+const g = e - f;
+const h = 7;
+const result2 = g + h;
+console.log(result2);
+
+
+// 4 - misol
+
+const i = 10;
+const j = 3;
+console.log(i % j);
+
+// 5 - misol
+
+
+const result3 = Math.pow(2, 10);
+console.log(result3);
